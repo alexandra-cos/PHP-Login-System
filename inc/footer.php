9ce0e7e3c375d14f99506d8f5cdb1abc3591d8d1
alexandra-cos <alexandraacostache@gmail.com>
2020-08-10 08:12:10 -0400

    Added config; added login/reg pages; added footer

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+<?php
+  //if there is no constant defined called _CONFIG_, do not load this file
+  /*if(!defined('_CONFIG_')){ //can define another constant in config file and check here for more security
+    exit('You do not have a config file.');
+  }*/ //not really needed because this code is not really secretive
+?>
+
+<!-- jQuery is required (from TCWD) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit-icons.min.js"></script>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+  //allow the config
+  define('_CONFIG_', true);
+  //require the config
+  require_once "inc/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,40 +23,17 @@
   <body>
 
     <div class="uk-section uk-container">
-      <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-
-        <form class="uk-form-stacked js.login">
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">Email</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-                </div>
-            </div>
-
-            <div class="uk-margin">
-                <label class="uk-form-label" for="form-stacked-text">Password</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Enter Password">
-                </div>
-            </div>
-
-            <div class="uk-margin">
-                <button class="uk.button uk-button-default" type="submit">Login</button>
-            </div>
-
-        </form>
-
-      </div>
+      <?php
+        echo "Hello world! Today is: ";
+        echo date("Y m d");
+      ?>
+      <p>
+        <a href="php_login_system/login.php">Login</a>
+        <a href="php_login_system/register.php">Register</a>
+      </p>
     </div>
 
-
-
-    <!-- jQuery is required (from TCWD) -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit-icons.min.js"></script>
+    <?php require_once "inc/footer.php" ?>
   </body>
 
 </html>
