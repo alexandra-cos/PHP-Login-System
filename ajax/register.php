f79c5f26fd49f77e69cfe9f838ca0751dad00762
alexandra-cos <alexandraacostache@gmail.com>
2020-08-10 13:05:12 -0400

    Added registration via ajax

--- ajax/register.php
+++ ajax/register.php
@@ -7,22 +7,49 @@
 
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
     //always return JSON format
-    header('Content-Type: application/json');
+    //header('Content-Type: application/json');
 
     $return = [];
+
+    $email = Filter::String($_POST['email']);
     //Make sure user does not exist.
+    $findUser = $con->prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
+    $findUser->bindParam('email', $email, PDO::PARAM_STR);
+    $findUser->execute();
+
+    if($findUser->rowCount() == 1){
+      //user exists
+      //we can also check to see if they are able to log in
+      $return['error'] = "You already have an account.";
+      $return['is_logged_in'] = false;
+    } else {
+      //user does not exist. add them now
+
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      $addUser = $con->prepare("INSERT INTO users(email, password) VALUES(LOWER(:email), :password)");
+      $addUser->bindParam(':email', $email, PDO::PARAM_STR);
+      $addUser->bindParam(':password', $password, PDO::PARAM_STR);
+      $addUser->execute();
+
+      $user_id = $con->lastInsertId();
+
+      $_SESSION['user_id'] = (int) $user_id;
+
+      $return['redirect'] = '/dashboard.php?message=welcome';
+      $return['is_logged_in'] = true;
+    }
 
     //Make sure the user CAN be added AND is added
 
     //Return the proper information back to JavaScript to redirect us
 
-    $return['redirect'] = '/dashboard.php';
+    $return['redirect'] = '/dashboard.php'; //not sure
     $return['name'] = "Alex Costache";
 
     echo json_encode($return, JSON_PRETTY_PRINT); exit;
   } else {
     //die. kill the script. redirect the user. do something regardless
-    exit('test');
+    exit('Invalid URL');
   }
 
 
--- assests/js/main.js
+++ assests/js/main.js
@@ -30,15 +30,63 @@ $(document)
   })
   .done(function ajaxDone(data) {
     //Whatever data is
-    console.log(data);
     if(data.redirect !== undefined) {
       window.location = data.redirect;
+    } else if(data.error !== undefined) {
+      _error.text(data.error).show();
     }
-    alert(data.name);
   })
   .fail(function ajaxFailed(e) {
     //This failed
-    console.log(e);
+  })
+  .always(function ajaxAlwaysDoThis(data) {
+    //always do
+    console.log('Always');
+  })
+
+  return false;
+})
+
+
+//LOGIN-----------------------------------------------------------------------------------------------
+.on("submit", "form.js-login", function(event) {
+  event.preventDefault();
+
+  var _form = $(this);
+  var _error = $(".js-error", _form);
+
+  var data = {
+    email: $("input[type='email']", _form).val(),
+    password: $("input[type='password']", _form).val()
+  };
+
+  if(data.email.length < 6) {
+    _error.text("Please enter a valid email address.").show();
+    return false;
+  } else if (data.password.length < 11) {
+    _error.text("Please enter a passphrase that is at least 11 characters long.").show();
+    return false;
+  }
+  //assuming the code gets this far, we can start the ajax process
+  _error.hide();
+
+  $.ajax({
+    type: 'POST',
+    url: 'php_login_system/ajax/login.php',
+    data: data,
+    dataType: 'json',
+    async: true,
+  })
+  .done(function ajaxDone(data) {
+    //Whatever data is
+    if(data.redirect !== undefined) {
+      window.location = data.redirect;
+    } else if(data.error !== undefined) {
+      _error.text(data.error).show();
+    }
+  })
+  .fail(function ajaxFailed(e) {
+    //This failed
   })
   .always(function ajaxAlwaysDoThis(data) {
     //always do
@@ -46,12 +94,8 @@ $(document)
   })
 
 
-
-
   return false;
 })
-
-
 
 
 
